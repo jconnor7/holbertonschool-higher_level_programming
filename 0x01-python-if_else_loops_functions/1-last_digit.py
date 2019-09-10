a6d1373f20055b96dff329aa7088cc7287f3fa6e
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-10 20:25:51 +0000

    Complete source code and check if is between certain range of parameters with it's last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+#check to see if random number is negative
+if number < 0:
+    last = (abs(number) % 10) * -1
+    #grab the last digit of the random number
+else:
+    last = number % 10
+if last > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number,
+    last))
+elif last == 0:
+    print("Last digit of {} is {} and is 0".format(number, last))
+elif last < 6:
+    print("Last digit of {} is {} and is less than 6 and not 0"
+   .format(number, last))
