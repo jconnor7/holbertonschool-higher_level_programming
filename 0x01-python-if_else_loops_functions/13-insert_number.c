2f6ba9507d5ff289d0d9383bc7965a4c12afd73e
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-12 16:28:09 +0000

    function in C that inserts a number into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,47 +1,50 @@
 #include "lists.h"
 /**
- * insert_node - function in C that inserts a number into a sorted singly linked list
+ * insert_node - function in C that inserts a number into a sorted singly linked
+ * list
  * @head: linked list
  * @number: number for being allocated
  * Return: address of new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *newnode;
-	listint_t *cur; /* cursor used to traverse the list */
+    listint_t *newnode;
+    listint_t *cur; /* cursor used to traverse the list */
 
-	newnode = malloc(sizeof(listint_t));
+    newnode = malloc(sizeof(listint_t));
 
-	if (newnode == NULL)
-		return (NULL);
+    if (newnode == NULL)
+        return (NULL);
 
-	newnode->n = number;   /* put number into newnode */
-	newnode->next = NULL;  /*prevent garbage value errors */
+    newnode->n = number;   /* put number into newnode */
+    newnode->next = NULL;  /*prevent garbage value errors */
 
-	cur = *head;
+    cur = *head;
 
-	if (cur == NULL) /* check for linked list */
-	{
-		cur = newnode;
-		return (newnode);
-	}
+    if (cur == NULL) /* check for linked list */
+    {
+        *head = newnode;
+        return (newnode);
+    }
 
-	if (cur->n >= number) /* adds at the beginning of the list */
-	{
-		newnode->next = *head;
-		cur = newnode;
-		return (newnode);
-	}
+    if (cur->n >= number) /* adds at the beginning of the list */
+    {
+        newnode->next = *head;
+        *head = newnode;
+        return (newnode);
+    }
 
-	while (cur) /* walk on list and insert node*/
-	{
-		if (cur->n >= number)
-		{
-		    break;
-		}
-		cur = cur->next;
-	}
-    newnode->next = cur->next;
+    while (cur->next != NULL) /* walk on list and insert node*/
+    {
+        if (cur->next->n >= number)
+        {
+            newnode->next = cur->next;
+            cur->next = newnode;
+                return (newnode);
+        }
+        cur = cur->next;
+    }
+
     cur->next = newnode;
-return(newnode);
+        return (newnode);
 }
