c4dcdea95e0aabc35c9d98ddbf9b54a241a07287
johnconnor77 <jfcalleh@unal.edu.co>
2019-09-18 02:46:12 +0000

    Fixed functionality for entire functionat return

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if not my_list:
+        return None
     my_list.sort()
     max_val = my_list[len(my_list)-1]
-    return (None if not my_list else max_val)
+    return (max_val)
