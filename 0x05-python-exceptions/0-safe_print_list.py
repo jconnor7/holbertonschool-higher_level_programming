3ee0d6ef38c34038b7c28f93146ee72a1108da11
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-01 05:12:18 +0000

    correct output for printing data

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     for i in my_list:
         try:
             if i <= x:
-                print("{}".format(i), end=" ")
+                print(i, end=" ")
                 counter += 1
         except IndexError:
-            pass
+            break
     print()
     return counter
