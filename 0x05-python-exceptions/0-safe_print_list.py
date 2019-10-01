d5d437698819ae5beb3950a0dcd43215f57aaa15
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-01 05:03:59 +0000

    Fixed on edge case where i overpass range for x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for i in range(x):
+    for i in my_list:
         try:
             if i <= x:
-                print("{}".format(my_list[i]), end=" ")
+                print("{}".format(i), end=" ")
                 counter += 1
-        except(IndexError, TypeError):
+        except IndexError:
             pass
     print()
     return counter
