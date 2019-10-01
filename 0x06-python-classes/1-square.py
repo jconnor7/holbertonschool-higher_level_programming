e32d9799f8581e0967888816b72917a8492b30b4
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-01 18:17:23 +0000

    class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """ Creates a class called square"""
+    def __init__(self, size=0):
+        """ Creates a private instance attribute, size
+        Args:
+            - size: size of square
+        """
+        self.__size = size
