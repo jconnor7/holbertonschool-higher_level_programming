37e54abc1e0abd05a684666430c145ce09ea686e
johnconnor77 <jfcalleh@unal.edu.co>
2019-10-22 02:40:04 +0000

    Class BaseGeometry with Exception raised when area method is called

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """ Implement a BaseGeometry class"""
+
+    def area(self):
+        """Public instance method for area"""
+        raise Exception("area() is not implemented")
