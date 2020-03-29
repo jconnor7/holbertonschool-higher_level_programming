c729f22378325eca013835640d375f07145df482
johnconnor77 <jfcalleh@unal.edu.co>
2020-03-29 14:31:13 -0500

    pep8 fixed issues

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.4
-""" Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+""" Lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
