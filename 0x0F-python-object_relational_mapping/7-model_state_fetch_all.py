abde1035b4764b43c2197b969f868accdcbebaf2
Alafresh <868@holbertonschool.com>
2020-03-29 23:15:56 -0500

    Added Base.metadata and modify session

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     for state in session.query(State).order_by(State.id).all():
