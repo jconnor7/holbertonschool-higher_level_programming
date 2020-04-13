df74ab853eb654c7c90a39543a6f664dd809e3bc
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-13 00:39:50 -0500

    Delete trailing space

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Script that takes URL and request size of body
-
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script takes URL and GET rqst display body rspns
-
 curl -sL GET "$1"
