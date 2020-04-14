67613301b56c6ffa1c6af914c440aef93ac6e423
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-14 18:49:32 -0500

    Fixed options for command line

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept.
-curl -sI "$1" -X OPTIONS | grep 'Allow' | awk '{print  $2,$3,$4}'
+curl -sI -X OPTIONS "$1" | grep 'Allow' | awk '{print  $2,$3,$4}'
