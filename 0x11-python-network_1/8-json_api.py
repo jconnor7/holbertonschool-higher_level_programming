848674c47c45a3a7da78ad749215effa5b334799
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-16 16:49:38 -0500

    Takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with
+    the letter as a param
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    url = "0.0.0.0:5000/search_user"
+    letter = argv[1] if len(argv) == 2 else ""
+
+    data = {'q': letter}
+    req = requests.post(url, data)
+
+    type_req = req.headers.get("content-type")
+
+    if type_req != 'application/json':
+        print("Not a valid JSON")
+    else:
+        req_json = req.json()
+
+    if len(req_json) == 0:
+        print("No Result")
+    else:
+        print("[{}] {}".format(req_json['id'], req_json['name']))
