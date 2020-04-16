f3781bd74b3755e649ef4f4ce70d5f590c261764
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-15 21:27:41 -0500

    Added variable for pep8 issue

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,9 +14,11 @@ def find_peak(list_of_integers):
 
     peak = list_of_integers[size - 1]
 
+    list_int = list_of_integers
+
     for i in range(0, size - 1, 1):
 
         if 0 < i < size - 1:
-            if list_of_integers[i - 1] < list_of_integers[i] > list_of_integers[i + 1]:
-                peak = list_of_integers[i]
+            if list_int[i - 1] < list_int[i] > list_int[i + 1]:
+                peak = list_int[i]
     return peak
