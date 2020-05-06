f2ee463e9e6bc73103dc53f78b4a01a44306f9b7
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-05 23:58:17 -0500

    A function that executes a function x amount of times!

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// A function that executes a function x amount of times
+
+exports.callMeMoby = function (x, theFunction) {
+    let i = 0;
+    while (i < x) {
+        theFunction();
+        i++;
+    }
+};
