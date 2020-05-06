c99f4a206dc6092badd27abd8a7bd8f3b22edc16
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-06 00:00:59 -0500

    Semmistard issues fixed

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // A function that executes a function x amount of times
-
 exports.callMeMoby = function (x, theFunction) {
-    let i = 0;
-    while (i < x) {
-        theFunction();
-        i++;
-    }
+  let i = 0;
+  while (i < x) {
+    theFunction();
+    i++;
+  }
 };
