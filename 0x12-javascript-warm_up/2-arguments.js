f68111e143a5d62ab79d9b51458b388d61d0d1c6
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-05 19:30:13 -0500

    variables setting

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints a message depending of the number of arguments passed
 
-let len_argv = process.argv.length;
+const len_argv = process.argv.length;
 
 if (len_argv > 3) {
   console.log('Arguments found');
