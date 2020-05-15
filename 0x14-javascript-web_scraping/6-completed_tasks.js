ae6acea4dcef63c82ad8ff84e032d357eb5e8e95
johnconnor77 <jfcalleh@unal.edu.co>
2020-05-15 02:07:41 -0500

    A script that computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// A script that computes the number of tasks completed by user id
+
+const request = require('request');
+const url = process.argv[2] + '?completed=true';
+const newDict = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const jsonBody = JSON.parse(body);
+    for (const data of jsonBody) {
+      if (data.userId in newDict) {
+        newDict[data.userId] += 1;
+      } else {
+        newDict[data.userId] = 1;
+      }
+    }
+  }
+  console.log(newDict);
+});
